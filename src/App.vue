9e67a07ce3edab05a07305d4e4b4e994ff8d626e
sahrullah <mohammadsahrullah894@gmail.com>
2023-10-04 14:29:36 +0700

    dev: add floating button whatsapp

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,19 @@ import Popup from './components/popup/Popup.vue';
 <template>
   <div id="app">
     <router-view/>
+    <FloatingWaButton nomor="+6282247623501" message="" type="redirect" />
   </div>
 </template>
+
+<script>
+import FloatingWaButton from "@/components/widget/FloatingWaButton.vue";
+
+export default {
+  components: {
+    FloatingWaButton,
+  }
+}
+</script>
 
 <style>
 @import 'assets/css/style.css';
