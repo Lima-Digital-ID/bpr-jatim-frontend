efe62dafa66cad7cf9df04148bca750cea30b5ad
sahrullah <mohammadsahrullah894@gmail.com>
2023-10-10 16:47:56 +0700

    dev: responsive floating button

--- src/main.js
+++ src/main.js
@@ -4,10 +4,10 @@ import router from "./router";
 import axios from "axios";
 import VueAxios from "vue-axios";
 import { i18n } from "./plugins/i18n";
-
+import "./assets/css/animation.css";
 Vue.config.productionTip = false;
 Vue.use(VueAxios, axios);
-// Vue.prototype.$serverURL = "https://backoffice.bankumkm.id/"
+// Vue.prototype.$serverURL = "http://localhost:8000/";
 Vue.prototype.$serverURL = "https://backoffice.bankumkm.id/";
 Vue.prototype.$imgProfile = (imageName) => require("@/assets/" + imageName);
 new Vue({
